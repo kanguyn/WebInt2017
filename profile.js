16c241023ec85fc2e60d39ee920c84db0d0a7af9
Kim-Anh-Nhi Nguyen <kim-anh-nhi.nguyen@telecom-paristech.fr>
2018-01-22 18:17:34 +0100

    another form local storage (part 3)

--- profile.js
+++ profile.js
@@ -4,19 +4,41 @@ $( function() {
     }).disableSelection();
 } );
 
-var myForm = [];
-function saveForm(){
-	// Take each input's name and value and add it to the array we already created.
-	$('form input[type=text]').each(function(){
-		myForm.push({ name: this.name, value: this.value});
-	});
-}
-function debug(){
-	// Loop through our array and output the values.  These values should reflect what form inputs we have above and what their current values are.
-	for (var i = 0; i < myForm.length; i++) {
-		console.log(myForm[i].name+': '+myForm[i].value);
+var formData = {
+	
+	set : function (){
+		// (Re)create the myForm valiable
+		var myForm = [];
+		// Delete old data from localstorage
+		localStorage.removeItem('myForm');
+		// Take each input's name and value and add it to the array we already created.
+		$('form input[type=text]').each(function(){
+			// Push each input's value into the temporary variable
+			myForm.push({ name: this.name, value: this.value});
+		});
+		// Add the array to localStorage
+		localStorage.myForm = JSON.stringify(myForm);
+	},
+	get : function (){
+		// Is the form already stored within localStorage? If so, get it and copy it's contents over our myform array variable.
+		if(localStorage.myForm != undefined){
+			// Get the existing values out of localStorage
+			myForm = JSON.parse(localStorage.myForm);
+			// Loop through myForm
+			for (var i = 0; i < myForm.length; i++) {
+				// Populate the form with what data you have for it
+				$('[name='+myForm[i].name+']').val(myForm[i].value);
+			}
+		}
 	}
 }
+// Populate the form with whatever data already exists for it
+formData.get();
+// Watch for a change in a change in an input field. If there is a change, resave the form values.
+$("input").change( function() {
+	// If a change is detected, save the values.
+	formData.set();
+});
 
 
 /*
